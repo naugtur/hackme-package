041335c17480d79c1cf0019726a4e02f2e86bd15
naugtur <naugtur@gmail.com>
2026-09-11 15:38:39 +0200

    2

---
+++ examples/project-vulnerable/lavamoat/.runner.cjs
@@ -0,0 +1,372 @@
+#! /usr/bin/env node
+/// <reference path="../runner/make-run-script-wrapper.global.d.ts" />
+
+/* global makeRunScriptWrapper */
+
+const { spawnSync } = require('node:child_process')
+const fs = require('node:fs')
+const path = require('node:path')
+const { tmpdir } = require('node:os')
+const scriptName = process.env.npm_lifecycle_event
+const scriptPayload = process.argv[3]
+
+const pkgJsonPath = process.env.npm_package_json
+
+if (!pkgJsonPath) {
+  throw Error(
+    `[LavaMoat] FATAL: 'npm_package_json' environment variable is missing. A modern package manager is required.`
+  )
+}
+
+if (!scriptName) {
+  console.error(
+    `[LavaMoat] Warning: 'npm_lifecycle_event' environment variable is missing.`
+  )
+}
+
+const pkgJsonFolder = path.dirname(pkgJsonPath)
+const fallbackShell = process.platform === 'win32' ? 'cmd.exe' : '/bin/sh'
+const shellArgs = process.platform === 'win32' ? ['/d', '/s', '/c'] : ['-c']
+
+const pathBinMatcherString = `node_modules${path.sep}.bin`
+
+const wrapper = makeRunScriptWrapper(
+  {
+    scriptName,
+    scriptPayload,
+    projectRoot: pkgJsonFolder,
+    pathBinMatcher: (fragment) => fragment.endsWith(pathBinMatcherString),
+    customizePermissionsConfig: addMandatoryReads,
+    readScriptsConfig: () => {
+      try {
+        const pkgData = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf8'))
+        return pkgData.scriptsConfig
+      } catch {
+        return undefined
+      }
+    },
+  },
+  {
+    readFileSync: fs.readFileSync,
+    pathJoin: path.join,
+    pathDelimiter: path.delimiter,
+    tmpdir,
+    realpathSync: fs.realpathSync,
+  }
+)
+
+const customEnv = wrapper.processEnv(process.env)
+
+// Note: spawnSync is used here instead of process.execve because execve is
+// not available on Windows. Leaving this here to switch to execve in case other reasons to
+// drop Windows support arise.
+// process.execve(fallbackShell, [...shellArgs, scriptPayload], customEnv)
+
+const result = spawnSync(fallbackShell, [...shellArgs, scriptPayload], {
+  stdio: 'inherit',
+  env: customEnv,
+  cwd: pkgJsonFolder,
+})
+if (result.error) {
+  console.error(
+    `[LavaMoat wrapper failed to execute "${scriptName}"] ${result.error.message}`
+  )
+}
+if (result && 'status' in result) {
+  process.exit(result.status)
+} else {
+  process.exit(1)
+}
+
+/**
+ * @param {Record<string, boolean | string | string[]>} configOptions
+ * @param {NodeJS.ProcessEnv} _env
+ */
+function addMandatoryReads(configOptions, _env) {
+  if (!configOptions['--permission']) {
+    return
+  }
+  if (!configOptions['--allow-fs-read']) {
+    configOptions['--allow-fs-read'] = []
+  }
+  if (Array.isArray(configOptions['--allow-fs-read'])) {
+    configOptions['--allow-fs-read'].push(
+      '$npm_config_prefix',
+      '$npm_config_userconfig'
+    )
+  }
+}
+
+;;
+/// <reference path="./make-run-script-wrapper.global.d.ts" />
+
+/**
+ * @param {MakeRunScriptWrapperOptions} param0
+ * @param {MakeRunScriptWrapperIO} param1
+ * @returns {MakeRunScriptWrapper}
+ */
+function makeRunScriptWrapper(
+  {
+    scriptName,
+    scriptPayload: _scriptPayload, // might be useful to read in the future
+    projectRoot,
+    pathBinMatcher,
+    customizePermissionsConfig,
+    readScriptsConfig,
+  },
+  { readFileSync, pathJoin, pathDelimiter, tmpdir, realpathSync }
+) {
+  const DEFAULT_PERMISSION_KEY = '#default'
+
+  /** @param {string} filePath */
+  function readJsonFile(filePath) {
+    return JSON.parse(readFileSync(filePath, 'utf8'))
+  }
+
+  /**
+   * @param {object} opts
+   * @param {Record<string, string> | undefined} opts.scriptsConfig
+   * @param {string} [opts.scriptName]
+   * @param {string} opts.projectRoot
+   */
+  function readConfig({
+    scriptsConfig,
+    scriptName = DEFAULT_PERMISSION_KEY,
+    projectRoot,
+  }) {
+    if (!scriptsConfig) {
+      return {}
+    }
+    const configName =
+      scriptsConfig[scriptName] || scriptsConfig[DEFAULT_PERMISSION_KEY]
+
+    // config needs to be optional, because it's opt-in first and specifying a default turns it opt-out.
+    if (!configName) {
+      return {}
+    }
+    const configPath = pathJoin(projectRoot, configName)
+    let conf
+    try {
+      conf = readJsonFile(configPath)
+      if (typeof conf !== 'object' || conf === null) {
+        throw Error(`Expected an object, got ${typeof conf}`)
+      }
+    } catch (err) {
+      const message = err instanceof Error ? err.message : String(err)
+      throw Error(
+        `[LavaMoat] Error loading script config file "${configPath}": ${message}`,
+        { cause: err }
+      )
+    }
+    return conf
+  }
+
+  /**
+   * Adds features we'd want in Node.js permissions model and intend to
+   * eventually upstream
+   *
+   * @param {ConfigOptions} configOptions
+   * @param {NodeJS.ProcessEnv} env
+   * @returns {void}
+   */
+  function permissionsModelCompatibilityExtensions(configOptions, env) {
+    // 1. support env variables as values in allow-fs-*
+    /**
+     * @param {string} value
+     * @returns {string}
+     */
+    const replaceEnvVar = (value) => {
+      const envVarMatch = value.match(/^\$([A-Z_][A-Z0-9_]*)$/i)
+      if (envVarMatch) {
+        const envVarName = envVarMatch[1]
+        if (env[envVarName] !== undefined) {
+          return env[envVarName]
+        } else {
+          console.error(
+            `[LavaMoat] Environment variable "${envVarName}" referenced in config but not found in environment`
+          )
+        }
+      }
+      return value
+    }
+
+    for (const key of ['--allow-fs-read', '--allow-fs-write']) {
+      if (Array.isArray(configOptions[key])) {
+        configOptions[key] = configOptions[key].map(replaceEnvVar)
+      }
+    }
+
+    // 2. tmp write - crossplatform
+    if (configOptions['--allow-fs-tmp'] === true) {
+      delete configOptions['--allow-fs-tmp']
+      if (configOptions['--allow-fs-write']) {
+        if (typeof configOptions['--allow-fs-write'] === 'string') {
+          configOptions['--allow-fs-write'] = [
+            configOptions['--allow-fs-write'],
+          ]
+        }
+        if (configOptions['--allow-fs-write'] === true) {
+          return // none of this matters
+        }
+      } else {
+        // do this for both undefined and false
+        configOptions['--allow-fs-write'] = []
+      }
+      const tmp = tmpdir()
+      configOptions['--allow-fs-write'].push(tmp)
+      // because macos is being weird
+      const tmpRealPath = realpathSync(tmp)
+      if (tmpRealPath !== tmp) {
+        configOptions['--allow-fs-write'].push(tmpRealPath)
+      }
+    }
+  }
+
+  /** @param {ConfigOptions} configOptions */
+  function makeFlagsFromConfig(configOptions) {
+    return Object.entries(configOptions)
+      .map(([arg, value]) => {
+        if (typeof value === 'boolean') {
+          return value ? arg : ''
+        } else if (Array.isArray(value)) {
+          return value.map((v) => `${arg}="${v}"`).join(' ')
+        } else {
+          return `${arg}="${value}"`
+        }
+      })
+      .filter(Boolean)
+      .join(' ')
+  }
+
+  /**
+   * Checks the config obtained from package.json and puts it in as NODE_OPTIONS
+   *
+   * @param {string | undefined} existingOptions
+   * @param {ConfigOptions} configOptions
+   * @param {NodeJS.ProcessEnv} env
+   */
+  function installNodeOptions(existingOptions, configOptions, env) {
+    if (!configOptions) {
+      return existingOptions || ''
+    }
+
+    customizePermissionsConfig(configOptions, env)
+
+    permissionsModelCompatibilityExtensions(configOptions, env)
+
+    const confOption = makeFlagsFromConfig(configOptions)
+
+    return `${existingOptions || ''} ${confOption.trim()}`.trim()
+  }
+
+  /**
+   * Filter environment variables based on ban keywords
+   *
+   * @param {NodeJS.ProcessEnv} env
+   * @param {string} lavamoatDir
+   * @returns {NodeJS.ProcessEnv}
+   */
+  function filterEnv(env, lavamoatDir) {
+    const banFilePath = pathJoin(lavamoatDir, '.env.ban.json')
+    let banConfig
+
+    try {
+      banConfig = readJsonFile(banFilePath)
+    } catch (err) {
+      const message = err instanceof Error ? err.message : String(err)
+      console.error(
+        `[LavaMoat] Warning: Failed to read .env.ban.json: ${message}.`
+      )
+      return env
+    }
+    let banKeywords = []
+    try {
+      if (Array.isArray(banConfig)) {
+        banKeywords.push(...banConfig)
+      } else {
+        throw Error(`Expected .env.ban.json to contain an array of keywords`)
+      }
+    } catch (err) {
+      const message = err instanceof Error ? err.message : String(err)
+      console.error(
+        `[LavaMoat] Warning: Failed to read .env.ban.json: ${message}.`
+      )
+      return env
+    }
+
+    /** @type {NodeJS.ProcessEnv} */
+    const filteredEnv = {}
+    const bannedEnv = []
+    banKeywords = banKeywords.map((keyword) => keyword.toLowerCase())
+    for (const [key, value] of Object.entries(env)) {
+      if (
+        !key.toLowerCase().startsWith('npm_config') &&
+        banKeywords.some((keyword) => key.toLowerCase().includes(keyword))
+      ) {
+        bannedEnv.push(key)
+      } else {
+        filteredEnv[key] = value
+      }
+    }
+    if (bannedEnv.length > 0) {
+      console.error(
+        `[LavaMoat] Warning: The following environment variables were banned: ${bannedEnv.join(', ')}`
+      )
+    }
+    return filteredEnv
+  }
+
+  /** @param {string} PATH */
+  function envPathOpinions(PATH) {
+    const pathFragments = PATH.split(pathDelimiter)
+    // This is to eliminate bin confusion attacks.
+    // Find node_modules/.bin and remove it, put it on the end that gets looked up last when looking for a name in the path.
+
+    /** @type {string[]} */
+    const filteredFragments = []
+    /** @type {string[]} */
+    const nodeModulesBinFragments = []
+
+    for (const fragment of pathFragments) {
+      if (pathBinMatcher(fragment)) {
+        nodeModulesBinFragments.push(fragment)
+      } else {
+        filteredFragments.push(fragment)
+      }
+    }
+    // Why would there be multiple bin fragments? In a npm workspace, local bin and workspace root bin is added
+    filteredFragments.push(...nodeModulesBinFragments)
+    return filteredFragments.join(pathDelimiter)
+  }
+
+  return {
+    processEnv: (existingEnv) => {
+      const scriptsConfig = readScriptsConfig(projectRoot)
+      const config = readConfig({
+        scriptsConfig,
+        scriptName,
+        projectRoot,
+      })
+
+      // Smell: Windows environment variables are case-insensitive, but Node's process.env
+      // might expose it as 'Path' instead of 'PATH'. Checking both ensures it doesn't get wiped out.
+
+      const existingPath = existingEnv.PATH || existingEnv.Path || ''
+
+      const lavamoatDir = pathJoin(projectRoot, 'lavamoat')
+
+      const fixedEnv = {
+        ...filterEnv(existingEnv, lavamoatDir),
+        PATH: envPathOpinions(existingPath),
+        NODE_OPTIONS: installNodeOptions(
+          existingEnv.NODE_OPTIONS,
+          config.nodeOptions,
+          existingEnv
+        ),
+      }
+      return fixedEnv
+    },
+  }
+}
+
+
